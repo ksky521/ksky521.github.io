c46dbf7fa6502358508f42ac0b9d95dd0ef08339
三水清 <ksky521@gmail.com>
2015-11-29 20:34:56 +0800

    add nodepp

--- ci.sh
+++ ci.sh
@@ -1,5 +1,19 @@
 #! /bin/sh
 # 发布hexo
-hexo deploy --generate
+hexo clean
+hexo generate
 
 # 发布nodeppt
+nodeppt generate slide ./public/slide -a
+
+cd public
+
+rm -rf debug.log
+rm -rf slide/.git*
+
+git init
+git add -A
+date_str=`date "+DATE: %m/%d/%Y%nTIME: %H:%M:%S"`
+git commit -m "build with nodeppt on $date_str"
+git push -u git@github.com:ksky521/ksky521.github.io.git master:master --force
+
